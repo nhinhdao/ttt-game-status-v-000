c9c427f8572564c21829869b9ac8245ee4c356b9
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 18:19:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONSS constant
-WIN_COMBINATIONSS = [
+WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
   [6,7,8], # bottom row
