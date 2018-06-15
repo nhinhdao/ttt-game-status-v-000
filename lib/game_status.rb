d4151fbe93492a2defebbe8d6a2e297ad30dec94
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 19:41:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,6 +37,7 @@ def over?(board)
 end
 
 def winner(board)
+      binding.pry
       if won?(board) && board.detect {|index| index == "X"}
             "X"
       elsif won?(board) && board.detect {|index| index == "O"}
